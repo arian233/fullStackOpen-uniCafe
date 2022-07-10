5fbb499669463dc01c044a50e1e8dd41e3007e66
Arian Ansari <esfahanarian@gmail.com>
2022-07-09 18:40:00 -0700

    Fully functional now

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+
+
+const Button = ({ lable, handleClick }) => {
+  return (<button onClick={handleClick}>{lable}</button>)
+
+}
+
+const Statistics = (props) => {
+  let good = props.good
+  let bad = props.bad
+  let neutral = props.neutral
+
+  if (good === 0 && neutral === 0 && bad === 0) {
+    return (
+      <p>No feedback given</p>
+    )
+  }
+
+  return (
+    <>
+      <tr>all <td > {props.allClicks} </td></tr>
+      <tr>average <td> {(props.good - props.bad) / props.allClicks}</td></tr>
+      <tr>positive feedback <td> {props.good / props.allClicks} %</td></tr>
+    </>
+
+  )
+}
+
 
 function App() {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const [allClicks, setAll] = useState(0)
+
+  const handleGoodClick = () => {
+    setGood(good + 1)
+    setAll(allClicks + 1)
+  }
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1)
+    setAll(allClicks + 1)
+  }
+  const handleBadClick = () => {
+    setBad(bad + 1)
+    setAll(allClicks + 1)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Give feedback</h1>
+      <Button handleClick={handleGoodClick} lable='good' />
+      <Button handleClick={handleNeutralClick} lable='neutral' />
+      <Button handleClick={handleBadClick} lable='bad' />
+      <h1>Statistics</h1>
+      <tr>good <td> {good}</td></tr>
+      <tr>neutral <td> {neutral}</td></tr>
+      <tr>bad <td>{bad}</td></tr>
+      <Statistics good={good} bad={bad} neutral={neutral} allClicks={allClicks} />
+
     </div>
-  );
+  )
 }
 
 export default App;
